b33ce80d6e5c6d4e0c60d08831c86dec5d536fbb
flamemontgomery <drflamemontgomery@gmail.com>
2021-04-23 04:32:57 +1200

    removed len and added array_len

--- include/blackhole_funcs.h
+++ include/blackhole_funcs.h
@@ -2,7 +2,12 @@
 #ifndef BLACKHOLE_FUNCS_H
 #define BLACKHOLE_FUNCS_H
 
-#define len(a) ((int)(sizeof(a)/sizeof(*a)))
+#include <stdio.h>
+#include <iostream>
+
 #define round(a) ((int)(a+0.5))
+
+template <typename T, std::size_t N>
+std::size_t array_len(T(&)[N]);
 
 #endif
