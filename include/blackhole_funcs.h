ff629ec323256518fa2aa559e8313df0fa6acac2
drflamemontgomery <drflamemontgomery@gmail.com>
2021-08-05 05:25:39 +1200

    Bug Fix

--- include/blackhole_funcs.h
+++ include/blackhole_funcs.h
@@ -5,7 +5,11 @@
 #include <stdio.h>
 #include <iostream>
 
-#define round(a) ((int)(a+0.5))
+//#define round(a) ((int)(a+0.5))
+
+static inline int round(int a) {
+  return (int)(a+0.5);
+}
 
 template <typename T, std::size_t N>
 std::size_t array_len(T(&)[N]);
