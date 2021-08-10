98bfabd60d4d438265d390e42d67ff232a53d88a
drflamemontgomery <drflamemontgomery@gmail.com>
2021-08-11 05:47:38 +1200

    Added copyright

--- include/blackhole_funcs.h
+++ include/blackhole_funcs.h
@@ -1,3 +1,24 @@
+/*
+  Copyright (C) 2021, Ashton Warner
+  All rights reserved.
+  
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+*/
+
+/**
+ * \file blackhole_funcs.h
+ *
+ * A blackhole library for universal functions
+ */
+
 #pragma once
 #ifndef BLACKHOLE_FUNCS_H
 #define BLACKHOLE_FUNCS_H
@@ -5,13 +26,8 @@
 #include <stdio.h>
 #include <iostream>
 
-//#define round(a) ((int)(a+0.5))
-
 static inline int round(int a) {
   return (int)(a+0.5);
 }
-
-template <typename T, std::size_t N>
-std::size_t array_len(T(&)[N]);
 
 #endif
--- include/graphics/tilemap.h
+++ include/graphics/tilemap.h
@@ -1,3 +1,24 @@
+/*
+  Copyright (C) 2021, Ashton Warner
+  All rights reserved.
+  
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+*/
+
+/**
+ * \file tilemap.h
+ *
+ * A blackhole library class for turning tilemaps into images
+ */
+
 #pragma once
 
 #ifndef TILEMAP_H
