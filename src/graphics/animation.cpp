7216f1661319e2e3147b2bca36a4dda86d4c8841
drflamemontgomery <drflamemontgomery@gmail.com>
2021-08-12 05:23:23 +1200

    changed type of bool setFrame() to void setFrame()

--- src/graphics/animation.cpp
+++ src/graphics/animation.cpp
@@ -34,12 +34,12 @@ namespace blackhole::graphics {
 
   Animation::Animation(SpriteSheet* images, int frame, int* frames, int num_frames, float speed)
   {
-	this->images = images;
-	images->setFrame(frame);
-	this->frames = frames;
-	this->num_frames = num_frames;
-	printf("%d\n", num_frames);
-	this->speed = speed;
+    this->images = images;
+    images->setFrame(frame);
+    this->frames = frames;
+    this->num_frames = num_frames;
+    printf("%d\n", num_frames);
+    this->speed = speed;
   }
 
   Animation::~Animation() {
@@ -47,52 +47,52 @@ namespace blackhole::graphics {
   }
 
   void Animation::setX(float x) {
-	images->setX(x);
+    images->setX(x);
   }
 
   void Animation::setY(float y) {
-	images->setY(y);
+    images->setY(y);
   }
   
   float Animation::getX() {
-	return images->getX();
+    return images->getX();
   }
 
   float Animation::getY() {
-	return images->getY();
+    return images->getY();
   }
 
-  bool Animation::setFrame(int frame) {
-	return images->setFrame(frame);
+  void  Animation::setFrame(int frame) {
+    images->setFrame(frame);
   }
 
   int Animation::getFrame() {
-	return images->getFrame();
+    return images->getFrame();
   }
 
   void Animation::resetAnimation() {
-	this->time = 0;
+    this->time = 0;
   }
   
   void Animation::addTime(float time) {
-	this->time += time;
+    this->time += time;
     images->setFrame(frames[(int)(this->time / speed) % num_frames]);
   }
 
   SpriteSheet* Animation::getSpriteSheet() {
-	return images;
+    return images;
   }
 
   SDL_Texture* Animation::getTexture() {
-	return images->getTexture();
+    return images->getTexture();
   }
   
   SDL_Rect* Animation::getSrcRect() {
-	return images->getSrcRect();
+    return images->getSrcRect();
   }
 
   SDL_Rect* Animation::getDestRect() {
-	return images->getDestRect();
+    return images->getDestRect();
   }
 
 }
